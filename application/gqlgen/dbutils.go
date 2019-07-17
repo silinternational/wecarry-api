c67a420ab36603e1e20ed3735fd363a6f4796341
Unknown <baggerone@gmail.com>
2019-07-17 15:20:10 -0400

    Created Posts resolver

--- application/gqlgen/dbutils.go
+++ application/gqlgen/dbutils.go
@@ -41,13 +41,26 @@ func ConvertDBUserToGqlUser(dbUser models.User, ctx context.Context) (User, erro
 func ConvertDBPostToGqlPost(dbPost models.Post, ctx context.Context) (Post, error) {
 	dbID := strconv.Itoa(dbPost.ID)
 
+	stubAfter := "0000"
+	stubBefore := "9999"
+	stubCategory := "Eats"
 
 	newGqlPost := Post{
 		ID:        dbID,
 		Type: PostType(dbPost.Type),
+		Title: dbPost.Title,
+		Description: GetStringFromNullsString(dbPost.Description),
+		Destination: GetStringFromNullsString(dbPost.Destination),
+		Origin: GetStringFromNullsString(dbPost.Origin),
+		Size: dbPost.Size,
+		NeededAfter: &stubAfter, //GetStringFromNullsString(dbPost.NeededAfter),
+		NeededBefore: &stubBefore, //GetStringFromNullsString(dbPost.NeededBefore),
+		Category: &stubCategory, // GetStringFromNullsString(dbPost.Category),
+		Status: dbPost.Status,
 		CreatedAt: ConvertTimeToStringPtr(dbPost.CreatedAt),
 		UpdatedAt: ConvertTimeToStringPtr(dbPost.UpdatedAt),
 	}
+
 
 	return newGqlPost, nil
 }
