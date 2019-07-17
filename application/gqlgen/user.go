1377b16a57bc6d600b49352af39196915d490507
Schparky <3172830+Schparky@users.noreply.github.com>
2019-07-17 17:32:04 -0600

    add ConvertGqlUserToDbUser

--- application/gqlgen/user.go
+++ application/gqlgen/user.go
@@ -1,8 +1,11 @@
 package gqlgen
 
 import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/gobuffalo/nulls"
 	"github.com/silinternational/handcarry-api/domain"
 	"github.com/silinternational/handcarry-api/models"
+	"github.com/vektah/gqlparser/gqlerror"
 	"strconv"
 )
 
@@ -25,4 +28,24 @@ func ConvertDBUserToGqlUser(dbUser models.User) (User, error) {
 	}
 
 	return newGqlUser, nil
+}
+
+func ConvertGqlUserToDbUser(user User) (models.User, error) {
+	role := user.AdminRole
+	var dbRole string
+	if role == nil {
+		dbRole = ""
+	} else {
+		dbRole = role.String()
+	}
+
+	newDbUser := models.User{
+		Email:     user.Email,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Nickname:  user.Nickname,
+		AdminRole: nulls.NewString(dbRole),
+	}
+
+	return newDbUser, nil
 }
