7beb65600d88133a0cb5bd9f2b012a33f9c6aed8
Mark Tompsett <mtompset@hotmail.com>
2019-07-18 11:45:17 -0400

    Feature: Add sample posts data

--- application/grifts/db.go
+++ application/grifts/db.go
@@ -4,6 +4,8 @@ import (
 	uuid2 "github.com/gofrs/uuid"
 	"github.com/markbates/grift/grift"
 	"github.com/silinternational/handcarry-api/models"
+	"github.com/gobuffalo/nulls"
+	"time"
 )
 
 var _ = grift.Namespace("db", func() {
@@ -101,8 +103,7 @@ var _ = grift.Namespace("db", func() {
 				return err
 			}
 		}
-
-		/*
+/*
 		fixtureUserOrgs := []*models.UserOrganization{
 			{
 				ID:     1,
@@ -143,6 +144,96 @@ var _ = grift.Namespace("db", func() {
 			}
 		}
 		*/
+
+		postUuid1, _ := uuid2.NewV4()
+		postUuid2, _ := uuid2.NewV4()
+		postUuid3, _ := uuid2.NewV4()
+		postUuid4, _ := uuid2.NewV4()
+		postUuid5, _ := uuid2.NewV4()
+		fixturePosts := []*models.Post{
+			{
+				ID:           1,
+				CreatedByID:  1,
+				Type:         "request",
+				OrgID:        1,
+				Status:       "unfulfilled",
+				Title:        "Maple Syrup",
+				Destination:  nulls.NewString("Madrid, Spain"),
+				Size:         "Medium",
+				Uuid:         postUuid1,
+				ReceiverID:   nulls.NewInt(1),
+				NeededAfter:  time.Date(2019,time.July,19,0,0,0,0,time.UTC),
+				NeededBefore: time.Date(2019,time.August,3,0,0,0,0,time.UTC),
+				Category:     "Unknown",
+				Description:  nulls.NewString("Missing my good, old, Canadian maple syrupy goodness"),
+			},
+			{
+				ID:           2,
+				CreatedByID:  2,
+				Type:         "request",
+				OrgID:        1,
+				Status:       "unfulfilled",
+				Title:        "Jif Peanut Butter",
+				Destination:  nulls.NewString("JAARS, NC, USA"),
+				Size:         "Small",
+				Uuid:         postUuid2,
+				ReceiverID:   nulls.NewInt(2),
+				NeededBefore: time.Date(2019,time.August,3,0,0,0,0,time.UTC),
+				Category:     "Food",
+				Description:  nulls.NewString("Jiffy Peanut Butter goes on our daily bread!"),
+			},
+			{
+				ID:           3,
+				CreatedByID:  3,
+				Type:         "request",
+				OrgID:        1,
+				Status:       "unfulfilled",
+				Title:        "Burt's Bee's Lip Balm",
+				Destination:  nulls.NewString("Atlanta, GA, USA"),
+				Size:         "Tiny",
+				Uuid:         postUuid3,
+				ReceiverID:   nulls.NewInt(3),
+				NeededAfter:  time.Date(2019,time.July,18,0,0,0,0,time.UTC),
+				Category:     "Personal",
+				Description:  nulls.NewString("Please save me from having painfully cracked lips!"),
+			},
+			{
+				ID:           4,
+				CreatedByID:  4,
+				Type:         "request",
+				OrgID:        1,
+				Status:       "unfulfilled",
+				Title:        "Peanut Butter",
+				Destination:  nulls.NewString("Orlando, FL, USA"),
+				Size:         "Small",
+				Uuid:         postUuid4,
+				ReceiverID:   nulls.NewInt(4),
+				NeededAfter:  time.Date(2019,time.August,3,0,0,0,0,time.UTC),
+				NeededBefore: time.Date(2019,time.September,1,0,0,0,0,time.UTC),
+				Category:     "Food",
+				Description:  nulls.NewString("I already have chocolate, but I need peanut butter."),
+			},
+			{
+				ID:           5,
+				CreatedByID:  5,
+				Type:         "request",
+				OrgID:        2,
+				Status:       "unfulfilled",
+				Title:        "Altoids",
+				Size:         "Tiny",
+				Uuid:         postUuid5,
+				ReceiverID:   nulls.NewInt(5),
+				Category:     "Mints",
+				Description:  nulls.NewString("The original celebrated curiously strong mints"),
+			},
+		}
+
+		for _, posts := range fixturePosts {
+			err := models.DB.Create(posts)
+			if err != nil {
+				return err
+			}
+		}
 
 		return nil
 	})
