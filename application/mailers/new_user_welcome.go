27a4309845f07d64606d478853fbd4ec2edca6d5
Phillip <phillip.shipley@gmail.com>
2019-07-18 15:19:24 -0400

    remove smtp from mailer template

--- application/mailers/new_user_welcome.go
+++ application/mailers/new_user_welcome.go
@@ -1,20 +1,16 @@
 package mailers
 
-import (
-	"github.com/gobuffalo/buffalo/render"
-	"github.com/gobuffalo/buffalo/mail"
-)
-
 func SendNewUserWelcomes() error {
-	m := mail.NewMessage()
-
-	// fill in with your stuff:
-	m.Subject = "New User Welcome"
-	m.From = ""
-	m.To = []string{}
-	err := m.AddBody(r.HTML("new_user_welcome.html"), render.Data{})
-	if err != nil {
-		return err
-	}
-	return smtp.Send(m)
+	// m := mail.NewMessage()
+	//
+	// // fill in with your stuff:
+	// m.Subject = "New User Welcome"
+	// m.From = ""
+	// m.To = []string{}
+	// err := m.AddBody(r.HTML("new_user_welcome.html"), render.Data{})
+	// if err != nil {
+	// 	return err
+	// }
+	// return smtp.Send(m)
+	return nil
 }
