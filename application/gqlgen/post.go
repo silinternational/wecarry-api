434079eb865694e586fe6cc542f653c771fffe41
Unknown <baggerone@gmail.com>
2019-07-18 10:43:35 -0400

    Added first mutation

--- application/gqlgen/post.go
+++ application/gqlgen/post.go
@@ -1,6 +1,7 @@
 package gqlgen
 
 import (
+	"github.com/gobuffalo/nulls"
 	"github.com/silinternational/handcarry-api/domain"
 	"github.com/silinternational/handcarry-api/models"
 	"strconv"
@@ -12,6 +13,7 @@ func ConvertDBPostToGqlPost(dbPost models.Post) (Post, error) {
 
 	newGqlPost := Post{
 		ID:           dbID,
+		UUID:         dbPost.Uuid.String(),
 		Type:         PostType(dbPost.Type),
 		Title:        dbPost.Title,
 		Description:  GetStringFromNullsString(dbPost.Description),
@@ -27,4 +29,35 @@ func ConvertDBPostToGqlPost(dbPost models.Post) (Post, error) {
 	}
 
 	return newGqlPost, nil
+}
+
+func ConvertGqlNewPostToDBPost(gqlPost NewPost) (models.Post, error) {
+
+	createdByUser, err := models.FindUserByUUID(gqlPost.CreatedByID)
+	if err != nil {
+		return models.Post{}, err
+	}
+
+	org, err := models.FindOrgByUUID(gqlPost.OrgID)
+	if err != nil {
+		return models.Post{}, err
+	}
+
+	dbPost := models.Post{}
+
+	dbPost.CreatedByID = createdByUser.ID
+	dbPost.OrgID = org.ID
+	dbPost.Type = gqlPost.Type.String()
+	dbPost.Title = gqlPost.Title
+
+	dbPost.Description = nulls.NewString(*gqlPost.Description)
+	dbPost.Origin = nulls.NewString(*gqlPost.Origin)
+	dbPost.Size = gqlPost.Size
+
+	//dbPost.NeededAfter = domain.ConvertStringPtrToTime(gqlPost.NeededAfter)
+	//dbPost.NeededBefore = domain.ConvertStringPtrToTime(gqlPost.NeededBefore)
+	dbPost.Category = domain.ConvertStrPtrToString(gqlPost.Category)
+
+	return dbPost, nil
+
 }
