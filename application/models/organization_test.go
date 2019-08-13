4fe1454b8338b1d6c3251924b7b8556a17e31549
Schparky <3172830+Schparky@users.noreply.github.com>
2019-08-13 14:26:58 -0600

    TestCreateOrganization and TestValidateOrganization

--- application/models/organization_test.go
+++ application/models/organization_test.go
@@ -1,17 +1,22 @@
 package models
 
 import (
+	"fmt"
+	"github.com/gobuffalo/nulls"
 	"github.com/gofrs/uuid"
+	"github.com/silinternational/handcarry-api/domain"
+
 	"reflect"
 	"testing"
 )
 
-func TestFindOrgByUUID(t *testing.T) {
+func init() {
 	if err := BounceTestDB(); err != nil {
-		t.Errorf("BounceTestDB() failed: %v", err)
-		return
+		panic(fmt.Errorf("BounceTestDB() failed: %v", err))
 	}
+}
 
+func TestFindOrgByUUID(t *testing.T) {
 	// Load Organization test fixtures
 	orgUuidStr := "51b5321d-2769-48a0-908a-7af1d15083e2"
 	orgUuid1, _ := uuid.FromString(orgUuidStr)
@@ -70,7 +75,127 @@ func TestFindOrgByUUID(t *testing.T) {
 			}
 		})
 	}
+
+	// clean up
+	resetOrganizationsTable(t)
 }
 
-func Test(t *testing.T) {
+func TestCreateOrganization(t *testing.T) {
+	tests := []struct {
+		name    string
+		org     Organization
+		wantErr bool
+	}{
+		{
+			name: "full",
+			org: Organization{
+				Name:       "ACME",
+				Uuid:       domain.GetUuid(),
+				AuthType:   "saml2",
+				AuthConfig: "[]",
+				Url:        nulls.NewString("https://www.example.com"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "minimum",
+			org: Organization{
+				Name:       "Bits 'R' Us",
+				Uuid:       domain.GetUuid(),
+				AuthType:   "saml2",
+				AuthConfig: "[]",
+			},
+			wantErr: false,
+		},
+		{
+			name: "missing auth config",
+			org: Organization{
+				Name:     "Bits 'R' Us",
+				Uuid:     domain.GetUuid(),
+				AuthType: "saml2",
+			},
+			wantErr: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := DB.Create(&test.org)
+			if (test.wantErr == true) && (err == nil) {
+				t.Errorf("Expected an error, but did not get one")
+			} else if (test.wantErr == false) && (err != nil) {
+				t.Errorf("Unexpected error %v", err)
+			}
+		})
+	}
+
+	// clean up
+	resetOrganizationsTable(t)
+}
+
+func TestValidateOrganization(t *testing.T) {
+	tests := []struct {
+		name     string
+		org      Organization
+		wantErr  bool
+		errField string
+	}{
+		{
+			name: "minimum",
+			org: Organization{
+				ID:         1,
+				Name:       "Bits 'R' Us",
+				Uuid:       domain.GetUuid(),
+				AuthType:   "saml2",
+				AuthConfig: "[]",
+			},
+			wantErr: false,
+		},
+		{
+			name: "missing name",
+			org: Organization{
+				ID:         1,
+				Uuid:       domain.GetUuid(),
+				AuthType:   "saml2",
+				AuthConfig: "[]",
+			},
+			wantErr:  true,
+			errField: "name",
+		},
+		{
+			name: "missing uuid",
+			org: Organization{
+				ID:         1,
+				Name:       "Babelfish Warehouse",
+				AuthType:   "saml2",
+				AuthConfig: "[]",
+			},
+			wantErr:  true,
+			errField: "uuid",
+		},
+		{
+			name: "missing auth type",
+			org: Organization{
+				ID:         1,
+				Name:       "Babelfish Warehouse",
+				Uuid:       domain.GetUuid(),
+				AuthConfig: "[]",
+			},
+			wantErr:  true,
+			errField: "auth_type",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			vErr, _ := test.org.Validate(DB)
+			if test.wantErr {
+				if vErr.Count() == 0 {
+					t.Errorf("Expected an error, but did not get one")
+				} else if len(vErr.Get(test.errField)) == 0 {
+					t.Errorf("Expected an error on field %v, but got none (errors: %v)", test.errField, vErr.Errors)
+				}
+			} else if (test.wantErr == false) && (vErr.HasAny()) {
+				t.Errorf("Unexpected error: %v", vErr)
+			}
+		})
+	}
 }
