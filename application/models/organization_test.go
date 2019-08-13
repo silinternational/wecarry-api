c7b560681908ea485ef55b9f31a1b38db41e9159
Schparky <3172830+Schparky@users.noreply.github.com>
2019-08-13 08:40:25 -0600

    add test cases to TestFindOrgByUUID

--- application/models/organization_test.go
+++ application/models/organization_test.go
@@ -2,21 +2,23 @@ package models
 
 import (
 	"github.com/gofrs/uuid"
+	"reflect"
 	"testing"
 )
 
 func TestFindOrgByUUID(t *testing.T) {
-	_ = BounceTestDB()
-
-	orgUuidStr := "51b5321d-2769-48a0-908a-7af1d15083e2"
-	orgName := "ACME"
+	if err := BounceTestDB(); err != nil {
+		t.Errorf("BounceTestDB() failed: %v", err)
+		return
+	}
 
 	// Load Organization test fixtures
+	orgUuidStr := "51b5321d-2769-48a0-908a-7af1d15083e2"
 	orgUuid1, _ := uuid.FromString(orgUuidStr)
 	orgFix := Organizations{
 		{
 			ID:         1,
-			Name:       orgName,
+			Name:       "ACME",
 			Uuid:       orgUuid1,
 			AuthType:   "saml2",
 			AuthConfig: "[]",
@@ -27,11 +29,48 @@ func TestFindOrgByUUID(t *testing.T) {
 		return
 	}
 
-	org, err := FindOrgByUUID(orgUuidStr)
-	if err != nil {
-		t.Errorf("FindOrgByUUID() returned an error: %v", err)
+	type args struct {
+		uuid string
 	}
-	if org.Name != orgName {
-		t.Errorf("expected %v, found %v", orgName, org.Name)
+	tests := []struct {
+		name    string
+		args    args
+		want    Organization
+		wantErr bool
+	}{
+		{
+			name: "found",
+			args: args{orgUuidStr},
+			want: orgFix[0],
+		},
+		{
+			name:    "empty access token",
+			args:    args{""},
+			wantErr: true,
+		},
+		{
+			name:    "near match",
+			args:    args{"51b5321d-2769-48a0-908a-7af1d15083e3"},
+			wantErr: true,
+		},
 	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := FindOrgByUUID(test.args.uuid)
+			if test.wantErr {
+				if err == nil {
+					t.Errorf("Expected an error, but did not get one")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("FindOrgByUUID() returned an error: %v", err)
+				} else if reflect.DeepEqual(got, test.want) {
+					t.Errorf("found %v, expected %v", got, test.want)
+				}
+			}
+		})
+	}
+}
+
+func Test(t *testing.T) {
 }
