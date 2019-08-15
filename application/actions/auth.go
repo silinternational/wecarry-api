d6f530bedde17ffbff28f1011c98f2e219b0897d
Schparky <3172830+Schparky@users.noreply.github.com>
2019-08-15 14:38:59 -0600

    Revert "redirect to IdP"
    This reverts commit 46f5229b17c0913a029c83799f71e3e1f39d9769.

--- application/actions/auth.go
+++ application/actions/auth.go
@@ -119,7 +119,7 @@ func AuthLogin(c buffalo.Context) error {
 			RedirectURL: authResp.RedirectURL,
 		}
 
-		return c.Redirect(303, resp.RedirectURL)
+		return c.Render(200, render.JSON(resp))
 	}
 
 	// if we have an authuser, find or create user in local db and finish login
