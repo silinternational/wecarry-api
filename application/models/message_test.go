ed986fddadad70467da3d36c49ad1bd110cc555d
Unknown <baggerone@gmail.com>
2019-08-26 16:55:39 -0400

    A  buffalo test suite with external fixture function and

--- application/models/message_test.go
+++ application/models/message_test.go
@@ -1,7 +1,24 @@
 package models
 
-import "testing"
+import ()
 
-func Test_Message(t *testing.T) {
-	t.Fatal("This test needs to be implemented!")
+func (as *ModelSuite) TestMessage_GetSender() {
+	t := as.T()
+
+	messageFixtures := Fixtures_GetSender(t)
+
+	messages := messageFixtures.Messages
+	users := messageFixtures.Users
+
+	userResults, err := messages[1].GetSender([]string{"nickname", "last_name"})
+
+	if err != nil {
+		t.Errorf("unexpected error ... %v", err)
+		t.FailNow()
+	}
+
+	as.Equal(users[1].ID, userResults.ID, "Bad user ID")
+
+	as.Equal(users[1].Nickname, userResults.Nickname, "Bad user Nickname")
+
 }
