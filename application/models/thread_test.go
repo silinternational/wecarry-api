238a3557e165bb3948ad87589b9bb7610f45d468
Schparky <3172830+Schparky@users.noreply.github.com>
2019-08-28 15:23:22 -0600

    TestThread_Validate

--- application/models/thread_test.go
+++ application/models/thread_test.go
@@ -3,12 +3,9 @@ package models
 import (
 	"testing"
 
+	"github.com/gobuffalo/validate"
 	"github.com/silinternational/handcarry-api/domain"
 )
-
-func Test_Thread(t *testing.T) {
-	t.Fatal("This test needs to be implemented!")
-}
 
 func CreateThreadFixtures(t *testing.T, post Post) []Thread {
 	// Load Thread test fixtures
@@ -48,4 +45,54 @@ func CreateThreadFixtures(t *testing.T, post Post) []Thread {
 	}
 
 	return threads
+}
+
+func (ms *ModelSuite) TestThread_Validate() {
+	t := ms.T()
+	tests := []struct {
+		name     string
+		thread   Thread
+		want     *validate.Errors
+		wantErr  bool
+		errField string
+	}{
+		{
+			name: "minimum",
+			thread: Thread{
+				PostID: 1,
+				Uuid:   domain.GetUuid(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "missing post_id",
+			thread: Thread{
+				Uuid: domain.GetUuid(),
+			},
+			wantErr:  true,
+			errField: "post_id",
+		},
+		{
+			name: "missing uuid",
+			thread: Thread{
+				PostID: 1,
+			},
+			wantErr:  true,
+			errField: "uuid",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			vErr, _ := test.thread.Validate(DB)
+			if test.wantErr {
+				if vErr.Count() == 0 {
+					t.Errorf("Expected an error, but did not get one")
+				} else if len(vErr.Get(test.errField)) == 0 {
+					t.Errorf("Expected an error on field %v, but got none (errors: %v)", test.errField, vErr.Errors)
+				}
+			} else if (test.wantErr == false) && (vErr.HasAny()) {
+				t.Errorf("Unexpected error: %v", vErr)
+			}
+		})
+	}
 }
