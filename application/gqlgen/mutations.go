a6cf522fa58d6aa65c1f592bd14211a53ca5e30c
Schparky <3172830+Schparky@users.noreply.github.com>
2019-09-06 09:55:28 -0600

    move post mutations to gqlgen/post.go

--- application/gqlgen/mutations.go
+++ application/gqlgen/mutations.go
@@ -9,54 +9,6 @@ import (
 
 type mutationResolver struct{ *Resolver }
 
-type postInput struct {
-	ID           *string
-	Status       *PostStatus
-	OrgID        *string
-	Type         *PostType
-	Title        *string
-	Description  *string
-	Destination  *string
-	Origin       *string
-	Size         *string
-	NeededAfter  *string
-	NeededBefore *string
-	Category     *string
-	URL          *string
-	Cost         *string
-}
-
-func (r *mutationResolver) CreatePost(ctx context.Context, input postInput) (*models.Post, error) {
-	cUser := models.GetCurrentUserFromGqlContext(ctx, TestUser)
-	post, err := convertGqlPostInputToDBPost(input, cUser)
-	if err != nil {
-		domain.Error(models.GetBuffaloContextFromGqlContext(ctx), err.Error(), domain.NoExtras)
-		return &models.Post{}, err
-	}
-
-	if err := models.DB.Create(&post); err != nil {
-		return &models.Post{}, err
-	}
-
-	return &post, nil
-}
-
-func (r *mutationResolver) UpdatePost(ctx context.Context, input postInput) (*models.Post, error) {
-	cUser := models.GetCurrentUserFromGqlContext(ctx, TestUser)
-	post, err := convertGqlPostInputToDBPost(input, cUser)
-	if err != nil {
-		domain.Error(models.GetBuffaloContextFromGqlContext(ctx), err.Error(), domain.NoExtras)
-		return &models.Post{}, err
-	}
-
-	if err := models.DB.Update(&post); err != nil {
-		domain.Error(models.GetBuffaloContextFromGqlContext(ctx), err.Error(), domain.NoExtras)
-		return &models.Post{}, err
-	}
-
-	return &post, nil
-}
-
 func (r *mutationResolver) CreateMessage(ctx context.Context, input NewMessage) (*models.Message, error) {
 	cUser := models.GetCurrentUserFromGqlContext(ctx, TestUser)
 	message, err := ConvertGqlNewMessageToDBMessage(input, cUser)
--- application/gqlgen/post.go
+++ application/gqlgen/post.go
@@ -290,3 +290,51 @@ func scopeUserOrgs(cUser models.User) pop.ScopeFunc {
 		return q.Where("organization_id IN (?)", s...)
 	}
 }
+
+type postInput struct {
+	ID           *string
+	Status       *PostStatus
+	OrgID        *string
+	Type         *PostType
+	Title        *string
+	Description  *string
+	Destination  *string
+	Origin       *string
+	Size         *string
+	NeededAfter  *string
+	NeededBefore *string
+	Category     *string
+	URL          *string
+	Cost         *string
+}
+
+func (r *mutationResolver) CreatePost(ctx context.Context, input postInput) (*models.Post, error) {
+	cUser := models.GetCurrentUserFromGqlContext(ctx, TestUser)
+	post, err := convertGqlPostInputToDBPost(input, cUser)
+	if err != nil {
+		domain.Error(models.GetBuffaloContextFromGqlContext(ctx), err.Error(), domain.NoExtras)
+		return &models.Post{}, err
+	}
+
+	if err := models.DB.Create(&post); err != nil {
+		return &models.Post{}, err
+	}
+
+	return &post, nil
+}
+
+func (r *mutationResolver) UpdatePost(ctx context.Context, input postInput) (*models.Post, error) {
+	cUser := models.GetCurrentUserFromGqlContext(ctx, TestUser)
+	post, err := convertGqlPostInputToDBPost(input, cUser)
+	if err != nil {
+		domain.Error(models.GetBuffaloContextFromGqlContext(ctx), err.Error(), domain.NoExtras)
+		return &models.Post{}, err
+	}
+
+	if err := models.DB.Update(&post); err != nil {
+		domain.Error(models.GetBuffaloContextFromGqlContext(ctx), err.Error(), domain.NoExtras)
+		return &models.Post{}, err
+	}
+
+	return &post, nil
+}
