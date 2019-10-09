ca7adb90b85dd415d543553a6e455d81283668db
Schparky <3172830+Schparky@users.noreply.github.com>
2019-10-09 12:35:04 -0600

    filled out more fields in the Post GQL query

--- application/gqlgen/post_test.go
+++ application/gqlgen/post_test.go
@@ -3,6 +3,7 @@ package gqlgen
 import (
 	"fmt"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	"time"
 
@@ -16,11 +17,11 @@ import (
 )
 
 type PostQueryFixtures struct {
-	Posts       models.Posts
-	Users       models.Users
-	Files       models.Files
-	ClientID    string
-	AccessToken string
+	models.Organization
+	models.Users
+	models.Posts
+	models.Files
+	models.Threads
 }
 
 func getGqlClient() *client.Client {
@@ -30,115 +31,77 @@ func getGqlClient() *client.Client {
 	return c
 }
 
-func Fixtures_PostQuery(t *testing.T) PostQueryFixtures {
-	// Load Org test fixtures
-	org := &models.Organization{
-		Name:       "TestOrg1",
-		Url:        nulls.String{},
-		AuthType:   models.AuthTypeSaml,
-		AuthConfig: "{}",
-		Uuid:       domain.GetUuid(),
-	}
-	err := models.DB.Create(org)
+func createFixture(t *testing.T, f interface{}) {
+	err := models.DB.Create(f)
 	if err != nil {
-		t.Errorf("could not create organization for test, error: %s", err)
+		t.Errorf("error creating %T fixture, %s", f, err)
 		t.FailNow()
 	}
+}
 
-	// Load User test fixtures
+func Fixtures_PostQuery(t *testing.T) PostQueryFixtures {
+	org := models.Organization{Uuid: domain.GetUuid(), AuthConfig: "{}"}
+	createFixture(t, &org)
+
 	users := models.Users{
-		{
-			Email:     "user1@example.com",
-			FirstName: "First",
-			LastName:  "User",
-			Nickname:  "User1",
-			Uuid:      domain.GetUuid(),
-			AdminRole: nulls.NewString(domain.AdminRoleSuperDuperAdmin),
-		},
-		{
-			Email:     "user2@example.com",
-			FirstName: "Second",
-			LastName:  "User",
-			Nickname:  "User2",
-			Uuid:      domain.GetUuid(),
-		},
+		{Email: t.Name() + "_user1@example.com", Nickname: t.Name() + " User1 ", Uuid: domain.GetUuid()},
+		{Email: t.Name() + "_user2@example.com", Nickname: t.Name() + " User2 ", Uuid: domain.GetUuid()},
 	}
-
 	for i := range users {
-		if err := models.DB.Create(&users[i]); err != nil {
-			t.Errorf("could not create test user ... %v", err)
-			t.FailNow()
-		}
+		createFixture(t, &(users[i]))
 	}
 
-	// Load UserOrganization test fixtures
 	userOrgs := models.UserOrganizations{
-		{
-			OrganizationID: org.ID,
-			UserID:         users[0].ID,
-			AuthID:         "auth_user1",
-			AuthEmail:      users[0].Email,
-		},
-		{
-			OrganizationID: org.ID,
-			UserID:         users[1].ID,
-			AuthID:         "auth_user2",
-			AuthEmail:      users[1].Email,
-		},
+		{OrganizationID: org.ID, UserID: users[0].ID, AuthID: t.Name() + "_auth_user1", AuthEmail: users[0].Email},
+		{OrganizationID: org.ID, UserID: users[1].ID, AuthID: t.Name() + "_auth_user2", AuthEmail: users[1].Email},
 	}
-
 	for i := range userOrgs {
-		if err := models.DB.Create(&userOrgs[i]); err != nil {
-			t.Errorf("could not create test user org ... %v", err)
-			t.FailNow()
-		}
+		createFixture(t, &(userOrgs[i]))
 	}
 
-	clientID := "12345678"
-	accessToken := "ABCDEFGHIJKLMONPQRSTUVWXYZ123456"
-	hash := models.HashClientIdAccessToken(clientID + accessToken)
-
-	userAccessToken := models.UserAccessToken{
-		UserID:             users[0].ID,
-		UserOrganizationID: userOrgs[0].ID,
-		AccessToken:        hash,
-		ExpiresAt:          time.Now().Add(time.Hour),
-	}
-
-	if err := models.DB.Create(&userAccessToken); err != nil {
-		t.Errorf("could not create test userAccessToken ... %v", err)
-		t.FailNow()
-	}
-
-	// Load Post test fixtures
 	posts := models.Posts{
 		{
-			CreatedByID:    users[0].ID,
-			Type:           PostTypeRequest.String(),
-			OrganizationID: org.ID,
-			Title:          "A Request",
-			Size:           PostSizeSmall.String(),
-			Status:         PostStatusOpen.String(),
 			Uuid:           domain.GetUuid(),
+			CreatedByID:    users[0].ID,
+			ReceiverID:     nulls.NewInt(users[0].ID),
 			ProviderID:     nulls.NewInt(users[1].ID),
+			OrganizationID: org.ID,
+			Type:           PostTypeRequest.String(),
+			Status:         PostStatusCommitted.String(),
+			Title:          "A Request",
+			Destination:    nulls.NewString("A place"),
+			Origin:         nulls.NewString("Another place"),
+			Size:           PostSizeSmall.String(),
+			NeededAfter:    time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+			NeededBefore:   time.Date(2099, 1, 1, 0, 0, 0, 0, time.UTC),
+			Category:       "OTHER",
+			Description:    nulls.NewString("This is a description"),
+			URL:            nulls.NewString("https://www.example.com/items/101"),
+			Cost:           nulls.NewFloat64(1.0),
 		},
 		{
-			CreatedByID:    users[0].ID,
-			Type:           PostTypeOffer.String(),
-			OrganizationID: org.ID,
-			Title:          "An Offer",
-			Size:           PostSizeLarge.String(),
-			Status:         PostStatusOpen.String(),
 			Uuid:           domain.GetUuid(),
-			ReceiverID:     nulls.NewInt(users[1].ID),
+			CreatedByID:    users[0].ID,
+			ProviderID:     nulls.NewInt(users[0].ID),
+			OrganizationID: org.ID,
 		},
 	}
-
 	for i := range posts {
-		if err := models.DB.Create(&posts[i]); err != nil {
-			t.Errorf("could not create test post ... %v", err)
-			t.FailNow()
-		}
+		createFixture(t, &(posts[i]))
+	}
+
+	threads := []models.Thread{
+		{Uuid: domain.GetUuid(), PostID: posts[0].ID},
+	}
+	for i := range threads {
+		createFixture(t, &(threads[i]))
+	}
+
+	threadParticipants := []models.ThreadParticipant{
+		{ThreadID: threads[0].ID, UserID: posts[0].CreatedByID},
+	}
+	for i := range threadParticipants {
+		createFixture(t, &(threadParticipants[i]))
 	}
 
 	if err := aws.CreateS3Bucket(); err != nil {
@@ -146,19 +109,12 @@ func Fixtures_PostQuery(t *testing.T) PostQueryFixtures {
 		t.FailNow()
 	}
 
-	// create file fixtures
 	fileData := []struct {
 		name    string
 		content []byte
 	}{
-		{
-			name:    "photo.gif",
-			content: []byte("GIF89a"),
-		},
-		{
-			name:    "dummy.pdf",
-			content: []byte("%PDF-"),
-		},
+		{"photo.gif", []byte("GIF89a")},
+		{"dummy.pdf", []byte("%PDF-")},
 	}
 	fileFixtures := make([]models.File, len(fileData))
 	for i, fileDatum := range fileData {
@@ -170,24 +126,22 @@ func Fixtures_PostQuery(t *testing.T) PostQueryFixtures {
 		fileFixtures[i] = f
 	}
 
-	// attach photo
-	if _, err := posts[1].AttachPhoto(fileFixtures[0].UUID.String()); err != nil {
+	if _, err := posts[0].AttachPhoto(fileFixtures[0].UUID.String()); err != nil {
 		t.Errorf("failed to attach photo to post, %s", err)
 		t.FailNow()
 	}
 
-	// attach file
-	if _, err := posts[1].AttachFile(fileFixtures[1].UUID.String()); err != nil {
+	if _, err := posts[0].AttachFile(fileFixtures[1].UUID.String()); err != nil {
 		t.Errorf("failed to attach file to post, %s", err)
 		t.FailNow()
 	}
 
 	return PostQueryFixtures{
-		Posts:       posts,
-		Users:       users,
-		Files:       fileFixtures,
-		ClientID:    clientID,
-		AccessToken: accessToken,
+		Organization: org,
+		Users:        users,
+		Posts:        posts,
+		Files:        fileFixtures,
+		Threads:      threads,
 	}
 }
 
@@ -195,18 +149,65 @@ func (gs *GqlgenSuite) Test_PostQuery() {
 	t := gs.T()
 	models.ResetTables(t, models.DB)
 
-	queryFixtures := Fixtures_PostQuery(t)
-	userFixtures := queryFixtures.Users
-	postFixtures := queryFixtures.Posts
-	fileFixtures := queryFixtures.Files
-
+	f := Fixtures_PostQuery(t)
 	c := getGqlClient()
 
-	query := `{ post(id: "` + postFixtures[1].Uuid.String() + `") { id photo { id } files { id } }}`
+	query := `{ post(id: "` + f.Posts[0].Uuid.String() + `") 
+		{ 
+			id 
+		    type
+			title
+			description
+			destination
+			origin
+			size
+			neededAfter
+			neededBefore
+			category
+			status
+			createdAt
+			updatedAt
+			myThreadID
+			cost
+			url
+			createdBy { id }
+			receiver { id }
+			provider { id }
+			organization { id }
+			photo { id }
+			files { id } 
+		}}`
 
-	var postsResp struct {
+	var resp struct {
 		Post struct {
-			ID    string `json:"id"`
+			ID           string `json:"id"`
+			Type         string `json:"type"`
+			Title        string `json:"title"`
+			Description  string `json:"description"`
+			Destination  string `json:"destination"`
+			Origin       string `json:"origin"`
+			Size         string `json:"size"`
+			NeededAfter  string `json:"neededAfter"`
+			NeededBefore string `json:"neededBefore"`
+			Category     string `json:"category"`
+			Status       string `json:"status"`
+			CreatedAt    string `json:"createdAt"`
+			UpdatedAt    string `json:"updatedAt"`
+			MyThreadID   string `json:"myThreadID"`
+			Cost         string `json:"cost"`
+			Url          string `json:"url"`
+			CreatedBy    struct {
+				ID string `json:"id"`
+			} `json:"createdBy"`
+			Receiver struct {
+				ID string `json:"id"`
+			} `json:"receiver"`
+			Provider struct {
+				ID string `json:"id"`
+			} `json:"provider"`
+			Organization struct {
+				ID string `json:"id"`
+			} `json:"organization"`
 			Photo struct {
 				ID string `json:"id"`
 			} `json:"photo"`
@@ -216,13 +217,34 @@ func (gs *GqlgenSuite) Test_PostQuery() {
 		} `json:"post"`
 	}
 
-	TestUser = userFixtures[0]
-	c.MustPost(query, &postsResp)
+	TestUser = f.Users[0]
+	c.MustPost(query, &resp)
 
-	gs.Equal(postFixtures[1].Uuid.String(), postsResp.Post.ID)
-	gs.Equal(fileFixtures[0].UUID.String(), postsResp.Post.Photo.ID)
-	gs.Equal(1, len(postsResp.Post.Files))
-	gs.Equal(fileFixtures[1].UUID.String(), postsResp.Post.Files[0].ID)
+	gs.Equal(f.Posts[0].Uuid.String(), resp.Post.ID)
+	gs.Equal(f.Posts[0].Type, resp.Post.Type)
+	gs.Equal(f.Posts[0].Title, resp.Post.Title)
+	gs.Equal(f.Posts[0].Description.String, resp.Post.Description)
+	gs.Equal(f.Posts[0].Destination.String, resp.Post.Destination)
+	gs.Equal(f.Posts[0].Origin.String, resp.Post.Origin)
+	gs.Equal(f.Posts[0].Size, resp.Post.Size)
+	gs.Equal(f.Posts[0].NeededAfter.Format(time.RFC3339), resp.Post.NeededAfter)
+	gs.Equal(f.Posts[0].NeededBefore.Format(time.RFC3339), resp.Post.NeededBefore)
+	gs.Equal(f.Posts[0].Category, resp.Post.Category)
+	gs.Equal(f.Posts[0].Status, resp.Post.Status)
+	gs.Equal(f.Posts[0].CreatedAt.Format(time.RFC3339), resp.Post.CreatedAt)
+	gs.Equal(f.Posts[0].UpdatedAt.Format(time.RFC3339), resp.Post.UpdatedAt)
+	cost, err := strconv.ParseFloat(resp.Post.Cost, 64)
+	gs.NoError(err, "couldn't parse cost field as a float")
+	gs.Equal(f.Posts[0].Cost.Float64, cost)
+	gs.Equal(f.Posts[0].URL.String, resp.Post.Url)
+	gs.Equal(f.Threads[0].Uuid.String(), resp.Post.MyThreadID)
+	gs.Equal(f.Users[0].Uuid.String(), resp.Post.CreatedBy.ID)
+	gs.Equal(f.Users[0].Uuid.String(), resp.Post.Receiver.ID)
+	gs.Equal(f.Users[1].Uuid.String(), resp.Post.Provider.ID)
+	gs.Equal(f.Organization.Uuid.String(), resp.Post.Organization.ID)
+	gs.Equal(f.Files[0].UUID.String(), resp.Post.Photo.ID)
+	gs.Equal(1, len(resp.Post.Files))
+	gs.Equal(f.Files[1].UUID.String(), resp.Post.Files[0].ID)
 }
 
 type UpdatePostFixtures struct {
