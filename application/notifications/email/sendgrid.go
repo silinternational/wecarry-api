9f6fac128c9b8d7ee21d7e694246f04e3c325e33
Schparky <3172830+Schparky@users.noreply.github.com>
2019-10-11 10:01:34 -0600

    simplify and rearrage

--- application/notifications/email/sendgrid.go
+++ application/notifications/email/sendgrid.go
@@ -20,50 +20,43 @@ type SendGridService struct {
 type sendGridTemplate struct {
 	// ID of SendGrid hosted template
 	id string
-
-	// these fields will not be needed if the SendGrid-hosted templates work well
-	subject, body string
 }
 
-var sendGridTemplateData = map[string]sendGridTemplate{
+var sendGridTemplates = map[string]sendGridTemplate{
 	domain.MessageTemplateNewMessage: {id: "d-3c43c00e9c384aff99260d53f1b0d482"},
-	domain.MessageTemplateNewRequest: {
-		subject: "new request",
-		body:    "There is a new request for an item from your location.",
-	},
+	domain.MessageTemplateNewRequest: {id: ""},
 }
 
 func (e *SendGridService) Send(msg Message) error {
 	apiKey := os.Getenv(domain.SendGridAPIKeyEnv)
-	templateData := sendGridTemplateData[msg.TemplateName]
-
-	message := mail.NewV3Mail()
-
-	message.SetTemplateID(templateData.id)
-	message.SetFrom(mail.NewEmail(msg.FromName, msg.FromEmail))
+	template, ok := sendGridTemplates[msg.TemplateName]
+	if !ok {
+		return fmt.Errorf("invalid message template name: %s", msg.TemplateName)
+	}
 
 	p := mail.NewPersonalization()
-	to := []*mail.Email{
-		mail.NewEmail(msg.ToName, msg.ToEmail),
-	}
-	p.AddTos(to...)
+	p.AddTos(mail.NewEmail(msg.ToName, msg.ToEmail))
 	for key, val := range msg.TemplateData {
 		p.SetDynamicTemplateData(key, val)
 	}
+
+	message := mail.NewV3Mail()
+	message.SetTemplateID(template.id)
+	message.SetFrom(mail.NewEmail(msg.FromName, msg.FromEmail))
 	message.AddPersonalizations(p)
 
-	fmt.Printf("data: %+v\n", message.Personalizations[0].DynamicTemplateData)
+	domain.Logger.Printf("email data: %+v\n", message.Personalizations[0].DynamicTemplateData)
 
 	request := sendgrid.GetRequest(apiKey, SendGridEndpointMailSend, SendGridAPIUrl)
 	request.Method = "POST"
 	request.Body = mail.GetRequestBody(message)
-	response, err := sendgrid.API(request)
 
+	response, err := sendgrid.API(request)
 	if err != nil {
 		return fmt.Errorf("error attempting to send message, %s", err)
 	}
 
-	if response.StatusCode != 202 {
+	if response.StatusCode >= 400 {
 		return fmt.Errorf("error response (%d) from sendgrid API, %s", response.StatusCode, response.Body)
 	}
 
