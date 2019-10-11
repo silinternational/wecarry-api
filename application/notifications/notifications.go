ed3bdf5b300e0a5efc887ac120bd080410ed0898
Schparky <3172830+Schparky@users.noreply.github.com>
2019-10-11 10:01:54 -0600

    use interface for notifiers

--- application/notifications/notifications.go
+++ application/notifications/notifications.go
@@ -2,30 +2,19 @@ package notifications
 
 import "github.com/silinternational/wecarry-api/domain"
 
-// Enabled controls which notifications are sent. Eventually this will be a per-user config.
-var Enabled struct {
-	Email, Mobile bool
-}
+var notifiers []Notifier
 
 func init() {
-	Enabled.Email = true
+	email := EmailNotifier{}
+	notifiers = append(notifiers, &email)
 }
 
 func Send(msg Message) error {
-	if Enabled.Email {
-		var e Email
-		if err := e.Send(msg); err != nil {
+	for _, n := range notifiers {
+		if err := n.Send(msg); err != nil {
 			return err
 		}
-		domain.Logger.Printf("%s message sent by email", msg.Template)
-	}
-
-	if Enabled.Mobile {
-		var m Mobile
-		if err := m.Send(msg); err != nil {
-			return err
-		}
-		domain.Logger.Printf("%s message sent by mobile", msg.Template)
+		domain.Logger.Printf("%T: %s message sent", n, msg.Template)
 	}
 
 	return nil
