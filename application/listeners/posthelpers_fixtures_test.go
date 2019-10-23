1874a7e1793c9409d3e154f369928d50a9839692
BaggerOne <baggerone@gmail.com>
2019-10-23 16:08:26 -0400

    Tests for new notification sending functions

--- application/listeners/posthelpers_fixtures_test.go
+++ application/listeners/posthelpers_fixtures_test.go
@@ -225,3 +225,110 @@ func CreateFixtures_RequestStatusUpdatedNotifications(ms *ModelSuite, t *testing
 		posts: posts,
 	}
 }
+
+func CreateFixtures_sendNotificationRequestFromStatus(ms *ModelSuite, t *testing.T) orgUserPostFixtures {
+
+	unique := domain.GetUuid().String()
+
+	// Load Organization test fixtures
+	org := m.Organization{
+		Name:       "ACME-" + unique,
+		Uuid:       domain.GetUuid(),
+		AuthType:   m.AuthTypeSaml,
+		AuthConfig: "{}",
+	}
+
+	if err := ms.DB.Create(&org); err != nil {
+		t.Errorf("error creating org %+v ...\n %v \n", org, err)
+		t.FailNow()
+	}
+
+	// Load User test fixtures
+	users := m.Users{
+		{
+			Email:     "user1-" + unique + "@example.com",
+			FirstName: "Existing1",
+			LastName:  "User",
+			Nickname:  "Existing1 User " + unique,
+			Uuid:      domain.GetUuid(),
+		},
+		{
+			Email:     "user2-" + unique + "@example.com",
+			FirstName: "Existing2",
+			LastName:  "User",
+			Nickname:  "Existing2 User " + unique,
+			Uuid:      domain.GetUuid(),
+		},
+		{
+			Email:     "not_participating-" + unique + "@example.com",
+			FirstName: "Not",
+			LastName:  "Participating",
+			Nickname:  "Not Participating " + unique,
+			Uuid:      domain.GetUuid(),
+		},
+	}
+	for i := range users {
+		if err := ms.DB.Create(&users[i]); err != nil {
+			t.Errorf("could not create test user %v ... %v", users[i], err)
+			t.FailNow()
+		}
+	}
+
+	// Load UserOrganization test fixtures
+	userOrg := m.UserOrganization{
+
+		OrganizationID: org.ID,
+		UserID:         users[0].ID,
+		AuthID:         users[0].Email,
+		AuthEmail:      users[0].Email,
+	}
+	if err := ms.DB.Create(&userOrg); err != nil {
+		t.Errorf("could not create test user org ... %v. uo = %+v", err, userOrg)
+		t.FailNow()
+	}
+
+	if err := m.DB.Load(&users[0], "Organizations"); err != nil {
+		t.Errorf("failed to load organizations on users[0] fixture, %s", err)
+	}
+
+	// Load Post test fixtures
+	posts := []m.Post{
+		{
+			CreatedByID:    users[0].ID,
+			Type:           m.PostTypeRequest,
+			OrganizationID: users[0].Organizations[0].ID,
+			Title:          "First Request",
+			Size:           m.PostSizeMedium,
+			Status:         m.PostStatusOpen,
+			Uuid:           domain.GetUuid(),
+			ProviderID:     nulls.NewInt(users[1].ID),
+			ReceiverID:     nulls.NewInt(users[0].ID),
+		},
+		{
+			CreatedByID:    users[0].ID,
+			Type:           m.PostTypeRequest,
+			OrganizationID: users[0].Organizations[0].ID,
+			Title:          "Second Request",
+			Size:           m.PostSizeMedium,
+			Status:         m.PostStatusOpen,
+			Uuid:           domain.GetUuid(),
+			ReceiverID:     nulls.NewInt(users[0].ID),
+		},
+	}
+	for i := range posts {
+		if err := ms.DB.Create(&posts[i]); err != nil {
+			t.Errorf("could not create test post ... %v", err)
+			t.FailNow()
+		}
+		if err := m.DB.Load(&posts[i], "CreatedBy", "Provider", "Receiver", "Organization"); err != nil {
+			t.Errorf("Error loading post associations: %s", err)
+			t.FailNow()
+		}
+	}
+
+	return orgUserPostFixtures{
+		orgs:  m.Organizations{org},
+		users: users,
+		posts: posts,
+	}
+}
--- application/listeners/posthelpers_test.go
+++ application/listeners/posthelpers_test.go
@@ -2,8 +2,11 @@ package listeners
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"github.com/silinternational/wecarry-api/domain"
 	"github.com/silinternational/wecarry-api/models"
+	"github.com/silinternational/wecarry-api/notifications"
 	"os"
 	"testing"
 )
@@ -65,7 +68,6 @@ func (ms *ModelSuite) TestRequestStatusUpdatedNotifications() {
 	t := ms.T()
 
 	orgUserPostFixtures := CreateFixtures_RequestStatusUpdatedNotifications(ms, t)
-	//users := orgUserPostFixtures.users
 	posts := orgUserPostFixtures.posts
 
 	postStatusEData := models.PostStatusEventData{
@@ -96,4 +98,76 @@ func (ms *ModelSuite) TestRequestStatusUpdatedNotifications() {
 	got = buf.String()
 	ms.Contains(got, "unexpected status transition 'OPEN-ACCEPTED'", "Got an unexpected error log entry")
 
+}
+
+// This returns an error that includes data that the tests can look for
+func dummySend(msg notifications.Message) error {
+	return errors.New(fmt.Sprintf("postTitle:%v", msg.Data["postTitle"]))
+}
+
+func (ms *ModelSuite) TestSendNotificationRequestFromStatus() {
+	t := ms.T()
+
+	ntfSend = dummySend
+
+	orgUserPostFixtures := CreateFixtures_sendNotificationRequestFromStatus(ms, t)
+	posts := orgUserPostFixtures.posts
+
+	var buf bytes.Buffer
+	domain.ErrLogger.SetOutput(&buf)
+
+	defer func() {
+		domain.ErrLogger.SetOutput(os.Stderr)
+	}()
+
+	tests := []struct {
+		name         string
+		post         models.Post
+		template     string
+		sendFunction func(string, models.Post)
+		wantContains string
+		wantEquals   string
+	}{
+		{name: "Good - Open to Committed",
+			post:         posts[0],
+			template:     domain.MessageTemplateRequestFromOpenToCommitted,
+			sendFunction: sendNotificationRequestFromCommittedToAccepted,
+			wantContains: "postTitle:" + posts[0].Title,
+		},
+		{name: "Bad - Open to Committed",
+			template:     domain.MessageTemplateRequestFromOpenToCommitted,
+			sendFunction: sendNotificationRequestFromCommittedToAccepted,
+			post:         posts[1],
+			wantEquals: fmt.Sprintf("error preparing '%s' notification - no provider\n",
+				domain.MessageTemplateRequestFromOpenToCommitted),
+		},
+		{name: "Good - Committed to Accepted",
+			post:         posts[0],
+			template:     domain.MessageTemplateRequestFromCommittedToAccepted,
+			sendFunction: sendNotificationRequestFromCommittedToAccepted,
+			wantContains: "postTitle:" + posts[0].Title,
+		},
+		{name: "Bad - Committed to Accepted",
+			template:     domain.MessageTemplateRequestFromCommittedToAccepted,
+			sendFunction: sendNotificationRequestFromCommittedToAccepted,
+			post:         posts[1],
+			wantEquals: fmt.Sprintf("error preparing '%s' notification - no provider\n",
+				domain.MessageTemplateRequestFromCommittedToAccepted),
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			test.sendFunction(test.template, test.post)
+			got := buf.String()
+			buf.Reset()
+
+			if test.wantContains != "" {
+				ms.Contains(got, test.wantContains, "missing data in fake test log entry")
+				return
+			}
+
+			ms.Equal(test.wantEquals, got, "wrong error log entry")
+		})
+	}
 }
