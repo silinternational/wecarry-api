8d0e7eec015c6d6a4ccf3b49ab6a0ee68bb7b3eb
BaggerOne <baggerone@gmail.com>
2019-11-14 09:18:42 -0500

    let notifications have its own renderer for emails

--- application/notifications/email.go
+++ application/notifications/email.go
@@ -1,5 +1,20 @@
 package notifications
 
+import (
+	"github.com/gobuffalo/buffalo/render"
+	"github.com/gobuffalo/packr/v2"
+)
+
+var r *render.Engine
+
+func init() {
+	r = render.New(render.Options{
+		HTMLLayout:   "layout.html",
+		TemplatesBox: packr.New("app:mailers:templates", "../templates/mail"),
+		Helpers:      render.Helpers{},
+	})
+}
+
 type EmailService interface {
 	Send(msg Message) error
 }
