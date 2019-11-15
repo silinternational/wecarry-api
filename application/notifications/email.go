8cbfdce55bed51e0b8980065634419bb8bba76be
BaggerOne <baggerone@gmail.com>
2019-11-15 13:54:44 -0500

    Just specify the weird cases and leave others as they are

--- application/notifications/email.go
+++ application/notifications/email.go
@@ -6,42 +6,27 @@ import (
 	"github.com/silinternational/wecarry-api/domain"
 )
 
-var r *render.Engine
-
-func init() {
-	r = render.New(render.Options{
-		HTMLLayout:   "layout.html",
-		TemplatesBox: packr.New("app:mailers:templates", "../templates/mail"),
-		Helpers:      render.Helpers{},
-	})
-}
+var eR = render.New(render.Options{
+	HTMLLayout:   "layout.html",
+	TemplatesBox: packr.New("app:mailers:templates", "../templates/mail"),
+	Helpers:      render.Helpers{},
+})
 
 type EmailService interface {
 	Send(msg Message) error
 }
 
-var emailTemplates = map[string]string{
-	domain.MessageTemplateNewThreadMessage:                domain.MessageTemplateNewThreadMessage,
-	domain.MessageTemplateNewRequest:                      domain.MessageTemplateNewRequest,
-	domain.MessageTemplateRequestFromCommittedToOpen:      domain.MessageTemplateRequestFromCommittedToOpen,
-	domain.MessageTemplateRequestFromAcceptedToOpen:       domain.MessageTemplateRequestFromAcceptedToOpen,
-	domain.MessageTemplateRequestFromOpenToCommitted:      domain.MessageTemplateRequestFromOpenToCommitted,
-	domain.MessageTemplateRequestFromCommittedToAccepted:  domain.MessageTemplateRequestFromCommittedToAccepted,
-	domain.MessageTemplateRequestFromDeliveredToAccepted:  domain.MessageTemplateRequestFromDeliveredToAccepted,
-	domain.MessageTemplateRequestFromReceivedToAccepted:   domain.MessageTemplateRequestFromReceivedToAccepted,
-	domain.MessageTemplateRequestFromCommittedToDelivered: "request_delivered",
-	domain.MessageTemplateRequestFromAcceptedToDelivered:  "request_delivered",
-	domain.MessageTemplateRequestFromReceivedToDelivered:  domain.MessageTemplateRequestFromReceivedToDelivered,
-	domain.MessageTemplateRequestFromCompletedToDelivered: domain.MessageTemplateRequestFromCompletedToDelivered,
-	domain.MessageTemplateRequestFromAcceptedToReceived:   domain.MessageTemplateRequestFromAcceptedToReceived,
-	domain.MessageTemplateRequestFromCompletedToReceived:  domain.MessageTemplateRequestFromCompletedToReceived,
-	domain.MessageTemplateRequestFromDeliveredToCompleted: domain.MessageTemplateRequestFromDeliveredToCompleted,
-	domain.MessageTemplateRequestFromReceivedToCompleted:  domain.MessageTemplateRequestFromReceivedToCompleted,
-	domain.MessageTemplateRequestFromOpenToRemoved:        domain.MessageTemplateRequestFromOpenToRemoved,
-	domain.MessageTemplateRequestFromCommittedToRemoved:   domain.MessageTemplateRequestFromCommittedToRemoved,
-	domain.MessageTemplateRequestFromAcceptedToRemoved:    domain.MessageTemplateRequestFromAcceptedToRemoved,
-}
-
 func GetEmailTemplate(key string) string {
-	return emailTemplates[key]
+
+	weirdTemplates := map[string]string{
+		domain.MessageTemplateRequestFromCommittedToDelivered: domain.MessageTemplateRequestDelivered,
+		domain.MessageTemplateRequestFromAcceptedToDelivered:  domain.MessageTemplateRequestDelivered,
+	}
+
+	template, ok := weirdTemplates[key]
+	if !ok {
+		template = key
+	}
+
+	return template
 }
