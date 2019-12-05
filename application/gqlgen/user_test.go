82723d92c7ca39ba3d2535ce1f159706a76c2e20
Schparky <3172830+Schparky@users.noreply.github.com>
2019-12-05 13:14:04 -0700

    Test_getPublicProfile

--- application/gqlgen/user_test.go
+++ application/gqlgen/user_test.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/nulls"
 	"github.com/silinternational/wecarry-api/aws"
 	"github.com/silinternational/wecarry-api/domain"
@@ -320,5 +321,56 @@ func (gs *GqlgenSuite) TestUpdateUser() {
 			gs.NoError(err)
 		}
 		t.Run(test.Name, test.Test)
+	}
+}
+
+func createFixturesForGetPublicProfile()UserQueryFixtures {
+	unique := domain.GetUuid().String()
+	user :=  models.User{
+		Uuid: domain.GetUuid(),
+		Nickname:     "user0" + unique,
+		AuthPhotoURL: nulls.NewString("https://example.com/userphoto/1"),
+	}
+	return UserQueryFixtures{Users: models.Users{user}}
+}
+
+func (gs *GqlgenSuite) Test_getPublicProfile() {
+	t := gs.T()
+	f := createFixturesForGetPublicProfile()
+	tests := []struct {
+		name string
+		user *models.User
+		want PublicProfile
+		wantNil bool
+	}{
+		{
+			name: "fully-specified User",
+			user: &f.Users[0],
+			want: PublicProfile{
+				ID:        f.Users[0].Uuid.String(),
+				Nickname:  f.Users[0].Nickname,
+				AvatarURL: &f.Users[0].AuthPhotoURL.String,
+			},
+		},
+		{
+			name: "nil user",
+			user: 	nil,
+			wantNil: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var ctx *buffalo.DefaultContext
+			profile := getPublicProfile(ctx, test.user)
+
+			if test.wantNil {
+				gs.Nil(profile)
+				return
+			}
+			gs.NotNil(profile)
+			gs.Equal(test.want.ID, profile.ID, "ID doesn't match")
+			gs.Equal(test.want.Nickname, profile.Nickname, "Nickname doesn't match")
+			gs.Equal(*test.want.AvatarURL, *profile.AvatarURL, "AvatarURL doesn't match")
+		})
 	}
 }
