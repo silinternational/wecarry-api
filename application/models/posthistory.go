b000f4bca9865fb1c295a083abfac62ab84c929d
BaggerOne <baggerone@gmail.com>
2019-12-10 10:41:19 -0500

    clarify error

--- application/models/posthistory.go
+++ application/models/posthistory.go
@@ -120,7 +120,7 @@ func (pH PostHistory) popForPost(post Post, currentStatus PostStatus) error {
 func (pH *PostHistory) getLastForPost(post Post) error {
 	if err := DB.Where("post_id = ?", post.ID).Last(pH); err != nil {
 		if domain.IsOtherThanNoRows(err) {
-			return err
+			return fmt.Errorf("error getting last Post History for post %v ... %v", post.ID, err)
 		}
 	}
 	return nil
