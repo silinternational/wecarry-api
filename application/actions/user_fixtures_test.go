32c61acc428fe0acc96900fc3082c9f3a33a4934
Schparky <3172830+Schparky@users.noreply.github.com>
2019-12-13 14:19:34 -0700

    use CreateUserFixtures in user query tests

--- application/actions/user_fixtures_test.go
+++ application/actions/user_fixtures_test.go
@@ -1,12 +1,10 @@
 package actions
 
 import (
-	"strconv"
-	"time"
-
 	"github.com/gobuffalo/nulls"
 	"github.com/silinternational/wecarry-api/aws"
 	"github.com/silinternational/wecarry-api/domain"
+	"github.com/silinternational/wecarry-api/internal/test"
 	"github.com/silinternational/wecarry-api/models"
 )
 
@@ -20,54 +18,13 @@ type UserQueryFixtures struct {
 }
 
 func fixturesForUserQuery(as *ActionSuite) UserQueryFixtures {
-	org := &models.Organization{AuthConfig: "{}", UUID: domain.GetUUID()}
-	createFixture(as, org)
+	uf := test.CreateUserFixtures(as.DB, as.T(), 2)
+	users := uf.Users
+	org := uf.Organization
 
-	locations := []models.Location{
-		{
-			Description: "Miami, FL, USA",
-			Country:     "US",
-			Latitude:    nulls.NewFloat64(25.7617),
-			Longitude:   nulls.NewFloat64(-80.1918),
-		},
-	}
-	for i := range locations {
-		createFixture(as, &locations[i])
-	}
-
-	unique := org.UUID.String()
-	users := models.Users{
-		{
-			AdminRole: models.UserAdminRoleSuperAdmin,
-		},
-		{
-			LocationID: nulls.NewInt(locations[0].ID),
-			AdminRole:  models.UserAdminRoleSalesAdmin,
-		},
-	}
-	userOrgs := make(models.UserOrganizations, len(users))
-	accessTokenFixtures := make([]models.UserAccessToken, len(users))
-	for i := range users {
-		users[i].UUID = domain.GetUUID()
-		users[i].Email = unique + "_user" + strconv.Itoa(i) + "@example.com"
-		users[i].Nickname = unique + "_auth_user" + strconv.Itoa(i)
-		users[i].FirstName = "first" + strconv.Itoa(i)
-		users[i].LastName = "last" + strconv.Itoa(i)
-		users[i].AuthPhotoURL = nulls.NewString("http://example.com/" + users[i].Nickname + ".gif")
-		createFixture(as, &users[i])
-
-		userOrgs[i].UserID = users[i].ID
-		userOrgs[i].OrganizationID = org.ID
-		userOrgs[i].AuthID = unique + "_auth_user" + strconv.Itoa(i)
-		userOrgs[i].AuthEmail = unique + users[i].Email
-		createFixture(as, &userOrgs[i])
-
-		accessTokenFixtures[i].UserID = users[i].ID
-		accessTokenFixtures[i].UserOrganizationID = userOrgs[i].ID
-		accessTokenFixtures[i].AccessToken = models.HashClientIdAccessToken(users[i].Nickname)
-		accessTokenFixtures[i].ExpiresAt = time.Now().Add(time.Minute * 60)
-		createFixture(as, &accessTokenFixtures[i])
-	}
+	users[0].AdminRole = models.UserAdminRoleSuperAdmin
+	users[1].AdminRole = models.UserAdminRoleSalesAdmin
+	as.NoError(as.DB.Save(&users))
 
 	// Load UserPreferences test fixtures
 	userPreferences := models.UserPreferences{
@@ -118,10 +75,10 @@ func fixturesForUserQuery(as *ActionSuite) UserQueryFixtures {
 	as.NoError(err)
 
 	return UserQueryFixtures{
-		Organization:    *org,
+		Organization:    org,
 		Users:           users,
 		UserPreferences: userPreferences,
 		Posts:           posts,
-		Locations:       locations,
+		Locations:       uf.Locations,
 	}
 }
