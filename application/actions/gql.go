967d4daf0e4610ca197aa1a3bb688fde34cc1381
Schparky <3172830+Schparky@users.noreply.github.com>
2019-12-19 11:49:22 -0700

    log gql queries

--- application/actions/gql.go
+++ application/actions/gql.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"context"
+	"io/ioutil"
 
 	"github.com/99designs/gqlgen/handler"
 	"github.com/gobuffalo/buffalo"
@@ -9,6 +10,11 @@ import (
 )
 
 func gqlHandler(c buffalo.Context) error {
+	body, err := ioutil.ReadAll(c.Request().Body)
+	if err == nil {
+		c.Logger().Printf("request body: %s", body)
+	}
+
 	h := handler.GraphQL(gqlgen.NewExecutableSchema(gqlgen.Config{Resolvers: &gqlgen.Resolver{}}))
 	newCtx := context.WithValue(c.Request().Context(), "BuffaloContext", c)
 	h.ServeHTTP(c.Response(), c.Request().WithContext(newCtx))
