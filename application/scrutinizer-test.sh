4f8d06872584a7d3b96800b7600aff8421ec75a7
Phillip <phillip.shipley@gmail.com>
2020-01-13 11:46:14 -0500

    install buffalo cli

--- application/scrutinizer-test.sh
+++ application/scrutinizer-test.sh
@@ -13,6 +13,11 @@ go get -v -u github.com/gobuffalo/suite
 go get -v -u github.com/gobuffalo/httptest
 go get -v -u github.com/markbates/grift
 
+# install Buffalo cli
+wget https://github.com/gobuffalo/buffalo/releases/download/v0.15.3/buffalo_0.15.3_Linux_x86_64.tar.gz
+tar -xvzf buffalo_0.15.3_Linux_x86_64.tar.gz
+chmod a+x buffalo
+
 # migrate db and run tests
 buffalo-pop pop migrate up
-buffalo test -coverprofile=coverage.out ./...
+./buffalo test -coverprofile=coverage.out ./...
