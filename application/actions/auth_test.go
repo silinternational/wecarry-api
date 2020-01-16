4ffa027d4028d08590332b533250e80a20ff1b47
BaggerOne <baggerone@gmail.com>
2020-01-16 08:38:00 -0500

    test VerifyEmails

--- application/actions/auth_test.go
+++ application/actions/auth_test.go
@@ -6,10 +6,57 @@ import (
 
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/buffalo/render"
+	"github.com/gobuffalo/logger"
+
 	"github.com/silinternational/wecarry-api/auth"
 	"github.com/silinternational/wecarry-api/domain"
 	"github.com/silinternational/wecarry-api/models"
 )
+
+func (as *ActionSuite) TestVerifyEmails() {
+	t := as.T()
+
+	tests := []struct {
+		name      string
+		authEmail string
+		respEmail string
+		wantErr   bool
+	}{
+		{
+			name:      "matching",
+			authEmail: "good@example.com",
+			respEmail: "good@example.com",
+		},
+		{
+			name:      "matching domains",
+			authEmail: "good@example.com",
+			respEmail: "other@example.com",
+		},
+		{
+			name:      "different domains",
+			authEmail: "good@example.com",
+			respEmail: "good@other.com",
+			wantErr:   true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			c := &bufTestCtx{
+				sess:   as.Session,
+				params: map[string]string{},
+			}
+
+			gotErr := verifyEmails(test.authEmail, test.respEmail, c)
+			if test.wantErr {
+				as.Error(gotErr, "missing expected error")
+				return
+			}
+			as.NoError(gotErr, "unexpected error")
+		})
+	}
+}
 
 func (as *ActionSuite) TestGetLoginSuccessRedirectURL() {
 	t := as.T()
@@ -110,6 +157,36 @@ func (b *bufTestCtx) Session() *buffalo.Session {
 
 func (b *bufTestCtx) Render(status int, r render.Renderer) error {
 	return fmt.Errorf("%v", status)
+}
+
+func (b *bufTestCtx) Value(key interface{}) interface{} {
+	return nil
+}
+
+func (b *bufTestCtx) Logger() logger.FieldLogger {
+	return testLogger{}
+}
+
+type testLogger struct{}
+
+func (l testLogger) Debugf(s string, i ...interface{}) {}
+func (l testLogger) Infof(s string, i ...interface{})  {}
+func (l testLogger) Printf(s string, i ...interface{}) {}
+func (l testLogger) Warnf(s string, i ...interface{})  {}
+func (l testLogger) Errorf(s string, i ...interface{}) {}
+func (l testLogger) Fatalf(s string, i ...interface{}) {}
+func (l testLogger) Debug(i ...interface{})            {}
+func (l testLogger) Info(i ...interface{})             {}
+func (l testLogger) Warn(i ...interface{})             {}
+func (l testLogger) Error(i ...interface{})            {}
+func (l testLogger) Fatal(i ...interface{})            {}
+func (l testLogger) Panic(i ...interface{})            {}
+
+func (l testLogger) WithField(s string, i interface{}) logger.FieldLogger {
+	return testLogger{}
+}
+func (l testLogger) WithFields(m map[string]interface{}) logger.FieldLogger {
+	return testLogger{}
 }
 
 func (as *ActionSuite) TestGetOrSetReturnTo() {
