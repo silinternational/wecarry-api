79fcb4237ae850e225a2a3f0a68b939df9114d22
BaggerOne <baggerone@gmail.com>
2020-02-03 11:42:28 -0500

    Cleaner way of checking DB connection

--- application/actions/status.go
+++ application/actions/status.go
@@ -10,8 +10,7 @@ import (
 
 // statusHandler is a handler to respond with the current site status
 func statusHandler(c buffalo.Context) error {
-	var orgs models.Organizations
-	if err := orgs.All(); err != nil {
+	if !models.IsDBConnected() {
 		return c.Render(http.StatusInternalServerError, r.JSON(map[string]string{"status": "error"}))
 	}
 	return c.Render(200, r.JSON(map[string]string{"status": "good"}))
