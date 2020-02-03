14403a66bc2b9f1cb6844264aa753431286de85a
BaggerOne <baggerone@gmail.com>
2020-02-03 08:26:09 -0500

    make status check touch db

--- application/actions/status.go
+++ application/actions/status.go
@@ -1,8 +1,18 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"net/http"
+
+	"github.com/gobuffalo/buffalo"
+
+	"github.com/silinternational/wecarry-api/models"
+)
 
 // statusHandler is a handler to respond with the current site status
 func statusHandler(c buffalo.Context) error {
+	var orgs models.Organizations
+	if err := orgs.All(); err != nil {
+		c.Render(http.StatusInternalServerError, r.JSON(map[string]string{"status": "error"}))
+	}
 	return c.Render(200, r.JSON(map[string]string{"status": "good"}))
 }
