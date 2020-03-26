b2e9cf8803ca6c1ff5f743f6f8dbfe82c42b1d79
Schparky <3172830+Schparky@users.noreply.github.com>
2020-03-26 16:25:09 -0600

    test Watch compare functions

--- application/models/watch_test.go
+++ application/models/watch_test.go
@@ -162,44 +162,6 @@ func (ms *ModelSuite) TestWatch_GetSetLocation() {
 	ms.InDelta(newLoc.Longitude.Float64, got.Longitude.Float64, 0.0001, "longitude doesn't match")
 }
 
-func (ms *ModelSuite) TestWatch_matchesPost() {
-	posts := createPostFixtures(ms.DB, 1, false)
-	watches := createWatchFixtures(ms.DB, createUserFixtures(ms.DB, 1).Users)
-
-	dest, err := posts[0].GetDestination()
-	ms.NoError(err)
-	dest.ID = 0
-	ms.NoError(dest.Create())
-	ms.NoError(watches[0].SetDestination(*dest))
-
-	ms.NoError(watches[1].SetDestination(Location{Country: "XX", Description: "-"}))
-
-	tests := []struct {
-		name  string
-		watch Watch
-		post  Post
-		want  bool
-	}{
-		{
-			name:  "match",
-			watch: watches[0],
-			post:  posts[0],
-			want:  true,
-		},
-		{
-			name:  "not match",
-			watch: watches[1],
-			post:  posts[0],
-			want:  false,
-		},
-	}
-	for _, tt := range tests {
-		ms.T().Run(tt.name, func(t *testing.T) {
-			ms.Equal(tt.want, tt.watch.matchesPost(tt.post))
-		})
-	}
-}
-
 func (ms *ModelSuite) TestWatch_Meeting() {
 	users := createUserFixtures(ms.DB, 2).Users
 	watches := createWatchFixtures(ms.DB, users)
@@ -242,6 +204,242 @@ func (ms *ModelSuite) TestWatch_Meeting() {
 			}
 			ms.NotNil(got, "Watch.Meeting() returned nil")
 			ms.Equal(tt.want.ID, got.ID)
+		})
+	}
+}
+
+func (ms *ModelSuite) TestWatch_compareDestination() {
+	posts := createPostFixtures(ms.DB, 1, false)
+	watches := createWatchFixtures(ms.DB, createUserFixtures(ms.DB, 1).Users)
+
+	dest, err := posts[0].GetDestination()
+	ms.NoError(err)
+	ms.NoError(dest.Create())
+	ms.NoError(watches[0].SetDestination(*dest))
+
+	ms.NoError(watches[1].SetDestination(Location{Country: "XX", Description: "-"}))
+
+	tests := []struct {
+		name  string
+		watch *Watch
+		post  Post
+		want  bool
+	}{
+		{
+			name:  "match",
+			watch: &watches[0],
+			post:  posts[0],
+			want:  true,
+		},
+		{
+			name:  "not match",
+			watch: &watches[1],
+			post:  posts[0],
+			want:  false,
+		},
+		{
+			name:  "nil",
+			watch: nil,
+			post:  posts[0],
+			want:  false,
+		},
+	}
+	for _, tt := range tests {
+		ms.T().Run(tt.name, func(t *testing.T) {
+			ms.Equal(tt.want, tt.watch.compareDestination(tt.post))
+		})
+	}
+}
+
+func (ms *ModelSuite) TestWatch_compareOrigin() {
+	posts := createPostFixtures(ms.DB, 1, false)
+	watches := createWatchFixtures(ms.DB, createUserFixtures(ms.DB, 1).Users)
+
+	origin, err := posts[0].GetOrigin()
+	ms.NoError(err)
+	ms.NoError(origin.Create())
+	ms.NoError(watches[0].SetOrigin(*origin))
+
+	ms.NoError(watches[1].SetOrigin(Location{Country: "XX", Description: "-"}))
+
+	tests := []struct {
+		name  string
+		watch *Watch
+		post  Post
+		want  bool
+	}{
+		{
+			name:  "match",
+			watch: &watches[0],
+			post:  posts[0],
+			want:  true,
+		},
+		{
+			name:  "not match",
+			watch: &watches[1],
+			post:  posts[0],
+			want:  false,
+		},
+		{
+			name:  "nil",
+			watch: nil,
+			post:  posts[0],
+			want:  false,
+		},
+	}
+	for _, tt := range tests {
+		ms.T().Run(tt.name, func(t *testing.T) {
+			ms.Equal(tt.want, tt.watch.compareOrigin(tt.post))
+		})
+	}
+}
+
+func (ms *ModelSuite) TestWatch_compareSize() {
+	posts := createPostFixtures(ms.DB, 1, false)
+	watches := createWatchFixtures(ms.DB, createUserFixtures(ms.DB, 1).Users)
+
+	postSize := posts[0].Size // PostSizeSmall
+	watches[0].Size = &postSize
+	ms.NoError(watches[0].Update())
+
+	tiny := PostSizeTiny
+	watches[1].Size = &tiny
+	ms.NoError(watches[1].Update())
+
+	tests := []struct {
+		name  string
+		watch *Watch
+		post  Post
+		want  bool
+	}{
+		{
+			name:  "match",
+			watch: &watches[0],
+			post:  posts[0],
+			want:  true,
+		},
+		{
+			name:  "not match",
+			watch: &watches[1],
+			post:  posts[0],
+			want:  false,
+		},
+		{
+			name:  "nil",
+			watch: nil,
+			post:  posts[0],
+			want:  false,
+		},
+	}
+	for _, tt := range tests {
+		ms.T().Run(tt.name, func(t *testing.T) {
+			ms.Equal(tt.want, tt.watch.compareSize(tt.post))
+		})
+	}
+}
+
+func (ms *ModelSuite) TestWatch_compareMeeting() {
+	posts := createPostFixtures(ms.DB, 1, false)
+	watches := createWatchFixtures(ms.DB, createUserFixtures(ms.DB, 1).Users)
+
+	// don't need to save these changes because compareMeeting doesn't access the database
+	watches[0].MeetingID = nulls.NewInt(1)
+	watches[1].MeetingID = nulls.NewInt(2)
+	posts[0].MeetingID = nulls.NewInt(1)
+
+	tests := []struct {
+		name  string
+		watch *Watch
+		post  Post
+		want  bool
+	}{
+		{
+			name:  "match",
+			watch: &watches[0],
+			post:  posts[0],
+			want:  true,
+		},
+		{
+			name:  "not match",
+			watch: &watches[1],
+			post:  posts[0],
+			want:  false,
+		},
+		{
+			name:  "nil",
+			watch: nil,
+			post:  posts[0],
+			want:  false,
+		},
+	}
+	for _, tt := range tests {
+		ms.T().Run(tt.name, func(t *testing.T) {
+			ms.Equal(tt.want, tt.watch.compareMeeting(tt.post))
+		})
+	}
+}
+
+func (ms *ModelSuite) TestWatch_compareText() {
+	posts := createPostFixtures(ms.DB, 1, false)
+	watches := createWatchFixtures(ms.DB, createUserFixtures(ms.DB, 4).Users)
+
+	postDescription := posts[0].Description.String
+	watches[0].SearchText = nulls.NewString(postDescription[:len(postDescription)-1])
+	ms.NoError(watches[0].Update())
+
+	postTitle := posts[0].Title
+	watches[1].SearchText = nulls.NewString(postTitle[:len(postTitle)-1])
+	ms.NoError(watches[1].Update())
+
+	postCreator, err := posts[0].Creator()
+	ms.NoError(err)
+	postCreatorNickname := postCreator.Nickname
+	watches[2].SearchText = nulls.NewString(postCreatorNickname[:5])
+	ms.NoError(watches[2].Update())
+
+	watches[3].SearchText = nulls.NewString("not a match for anything")
+	ms.NoError(watches[3].Update())
+
+	tests := []struct {
+		name  string
+		watch *Watch
+		post  Post
+		want  bool
+	}{
+		{
+			name:  "match description",
+			watch: &watches[0],
+			post:  posts[0],
+			want:  true,
+		},
+		{
+			name:  "match title",
+			watch: &watches[1],
+			post:  posts[0],
+			want:  true,
+		},
+		{
+			name:  "match nickname",
+			watch: &watches[2],
+			post:  posts[0],
+			want:  true,
+		},
+		{
+			name:  "not match",
+			watch: &watches[3],
+			post:  posts[0],
+			want:  false,
+		},
+		{
+			name:  "nil",
+			watch: nil,
+			post:  posts[0],
+			want:  false,
+		},
+	}
+	for _, tt := range tests {
+		ms.T().Run(tt.name, func(t *testing.T) {
+			ms.Equal(tt.want, tt.watch.compareText(tt.post))
 		})
 	}
 }
