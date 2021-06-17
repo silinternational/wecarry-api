d82b8635d33a317ae7143b52404443ca714da7c4
Schparky <3172830+Schparky@users.noreply.github.com>
2021-06-17 16:18:03 -0600

    don't need 'Message' yet

--- application/domain/domain.go
+++ application/domain/domain.go
@@ -269,9 +269,7 @@ type AppError struct {
 	// since these will be converted to human-friendly texts on the UI
 	Key string `json:"key"`
 
-	// Message providing detail about the error condition, only provided in development mode
 	DebugMsg string `json:"debug_msg,omitempty"`
-	Message  string
 }
 
 // ErrLogProxy wraps standard error logger plus sends to Rollbar
