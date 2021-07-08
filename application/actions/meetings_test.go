6a2465e2f4e6e3866f5226e32eefb700e9ceee9a
Steve Bagwell <steve_bagwell@sil.org>
2021-07-08 13:48:47 -0400

    PR suggestions

--- application/actions/meetings_test.go
+++ application/actions/meetings_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/silinternational/wecarry-api/api"
 	"github.com/silinternational/wecarry-api/domain"
 	"github.com/silinternational/wecarry-api/models"
+	"net/http"
 	"testing"
 )
 
@@ -66,35 +67,49 @@ func (as *ActionSuite) Test_MeetingsList() {
 func (as *ActionSuite) Test_meetingsJoin() {
 	f := createFixturesForMeetings(as)
 
+	mtgCreator := f.Users[0]
+
 	type testCase struct {
-		name       string
-		inviteCode string
-		location   models.Location
-		meeting    models.Meeting
-		user       models.User
-		wantInvite models.MeetingInvite
+		name            string
+		inviteCode      string
+		location        models.Location
+		meeting         models.Meeting
+		user            models.User
+		wantInvite      models.MeetingInvite
+		wantHTTPStatus  int
+		wantContainsErr string
 	}
 
 	testCases := []testCase{
 		{
-			name:     "already a participant",
-			location: f.Locations[2],
-			meeting:  f.Meetings[2],
-			user:     f.Users[2],
+			name:            "bad Meeting ID",
+			meeting:         models.Meeting{UUID: domain.GetUUID()},
+			user:            f.Users[0],
+			wantHTTPStatus:  http.StatusNotFound,
+			wantContainsErr: api.ErrorMeetingsGet.String(),
 		},
 		{
-			name:     "meeting creator",
-			location: f.Locations[0],
-			meeting:  f.Meetings[0],
-			user:     f.Users[0],
+			name:           "already a participant",
+			location:       f.Locations[2],
+			meeting:        f.Meetings[2],
+			user:           f.Users[2],
+			wantHTTPStatus: http.StatusOK,
 		},
 		{
-			name:       "regular user",
-			inviteCode: f.MeetingInvites[3].Secret.String(),
-			location:   f.Locations[3],
-			meeting:    f.Meetings[3],
-			user:       f.Users[1],
-			wantInvite: f.MeetingInvites[3],
+			name:           "meeting creator",
+			location:       f.Locations[0],
+			meeting:        f.Meetings[0],
+			user:           f.Users[0],
+			wantHTTPStatus: http.StatusOK,
+		},
+		{
+			name:           "regular user",
+			inviteCode:     f.MeetingInvites[3].Secret.String(),
+			location:       f.Locations[3],
+			meeting:        f.Meetings[3],
+			user:           f.Users[1],
+			wantInvite:     f.MeetingInvites[3],
+			wantHTTPStatus: http.StatusOK,
 		},
 	}
 
@@ -114,11 +129,16 @@ func (as *ActionSuite) Test_meetingsJoin() {
 			res := req.Post(reqBody)
 
 			body := res.Body.String()
-			as.Equal(200, res.Code, "incorrect status code returned, body: %s", body)
+			as.Equal(tc.wantHTTPStatus, res.Code, "incorrect status code returned, body: %s", body)
+			if tc.wantHTTPStatus != http.StatusOK {
+				as.Contains(body, tc.wantContainsErr, "missing error message")
+				return
+			}
 
 			wantContains := []string{
-				fmt.Sprintf(`"nickname":"%s"`, tc.user.Nickname),
+				fmt.Sprintf(`"created_by":{"id":"%s"`, mtgCreator.UUID),
 				fmt.Sprintf(`"participants":[{"user":{"id":"%s"`, tc.user.UUID.String()),
+				fmt.Sprintf(`"nickname":"%s"`, tc.user.Nickname),
 				fmt.Sprintf(`"id":"%s"`, tc.meeting.UUID.String()),
 				fmt.Sprintf(`"name":"%s"`, tc.meeting.Name),
 				fmt.Sprintf(`"start_date":"%s`, tc.meeting.StartDate.Format(domain.DateFormat)),
